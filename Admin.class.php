32af0c70c0e58d346551f2e87db21f146989493c
Alex <alexdeoiny@gmail.com>
2014-05-28 14:36:04 +0200

    maj

--- Admin.class.php
+++ Admin.class.php
@@ -7,32 +7,39 @@
 		public static function getQrCodeFromSet($id_set,$width,$height){ // return url
 			$query= "SELECT * FROM LM_set, LM_materiel WHERE LM_set.id_set = LM_materiel.id_set AND LM_set.id_set = $id_set";
 			$stmt = myPDO::getSingletonPDO()->query($query);
-
-			$chaine_qr = '';
-			while($ligne = $stmt->fetch()){
-				if($ligne['reference_campus'] != NULL){
-					$chaine_qr.=$ligne['reference_campus'].',';
-				}
-			}
-			$chaine_qr = preg_replace('/,$/','', $chaine_qr);
+			$ligne = $stmt->fetch();
+			if($ligne)	
+				$chaine_qr = 'id_set='.$ligne['id_set'];
 			return "http://chart.googleapis.com/chart?cht=qr&chs=".$width.'x'.$height.'&chl='.urlencode(utf8_encode($chaine_qr)).'&choe=UTF-8';
 		}
 
-		public static function getPdfFromSet($id_set,$width_qr,$height_qr){
+		public static function getPdfFromSet($pdf,$id_set,$width_qr,$height_qr){
 			$stmt = myPDO::getSingletonPDO()->query("SELECT description,id_set FROM LM_set WHERE id_set = $id_set");
 			if($ligne = $stmt->fetch()){
 				$description = utf8_decode($ligne['description']);
-				$pdf = new FPDF('P','mm','A4');
 				$pdf->AddPage();
 				$pdf->SetFont('Arial','B',16);
 				$pdf->MultiCell(0,10,'Set id : '.$ligne['id_set'],0,'C');
 				$pdf->MultiCell(0,10,$description,0,'C');
 				$url = self::getQrCodeFromSet($id_set,$width_qr,$height_qr);
 				$pdf->Image($url,60,100,90,0,'PNG');
-				$pdf->output();
 			}
+		}
+
+		public static function getAllSetQrCode(){
+			$query = "SELECT id_set, description FROM LM_set";
+			$stmt=myPDO::getSingletonPDO()->query($query);
+			$pdf = new FPDF('P','mm','A4');
+
+			while($ligne=$stmt->fetch()){
+				self::getPdfFromSet($pdf,$ligne['id_set'],547,547);
+			}
+				$pdf->output();
+			
+			$stmt->closeCursor();
 		}
 	}
 
 	//var_dump(Admin::genereQrCodeFromSet(1,500,500));
 	//Admin::getPdfFromSet(1,500,500);
+	Admin::getAllSetQrCode();
